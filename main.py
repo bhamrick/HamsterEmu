7325625da1e32f081e1cf70da6b7ac48d04586ce
Brian C Hamrick <bhamrick@mit.edu>
2013-03-27 03:45:38 -0400

    Reduced pause

--- main.py
+++ main.py
@@ -9,8 +9,8 @@ def main(rom_file):
         game.step_frame()
         print game.cpu.clock / 70224
         disp.update(game.gpu.pixels)
-        disp.root.after(500, advance)
-    disp.root.after(500, advance)
+        disp.root.after(1, advance)
+    disp.root.after(1, advance)
     disp.root.mainloop()
 
 if __name__ == "__main__":
